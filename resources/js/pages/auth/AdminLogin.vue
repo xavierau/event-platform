07d7c84e88ccbc054cb20d531038640a8115ce5a
Xavier Au <xavier.au@gmail.com>
2025-05-28 18:56:25 +0800

    update

--- resources/js/pages/auth/AdminLogin.vue
+++ resources/js/pages/auth/AdminLogin.vue
@@ -55,7 +55,7 @@ const submit = () => {
                 <div class="grid gap-2">
                     <div class="flex items-center justify-between">
                         <Label for="password">Password</Label>
-                        <TextLink v-if="canResetPassword" :href="route('admin.password.request')" class="text-sm" :tabindex="5">
+                        <TextLink v-if="canResetPassword" :href="route('password.request')" class="text-sm" :tabindex="5">
                             Forgot password?
                         </TextLink>
                     </div>
