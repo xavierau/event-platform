60ba48f92236b023538ae43ff35a3ee9b6c03eaf
Xavier Au <xavier.au@gmail.com>
2025-08-26 15:12:34 +0800

    update

--- resources/js/components/MemberScanner/MemberDetailsModal.vue
+++ resources/js/components/MemberScanner/MemberDetailsModal.vue
@@ -163,7 +163,7 @@
         <Button
           variant="default"
           @click="$emit('check-in')"
-          :disabled="isProcessing || checkInStatus?.success === true"
+          :disabled="!checkInForm.location || isProcessing || checkInStatus?.success === true"
         >
           <span v-if="isProcessing">
             <svg class="animate-spin -ml-1 mr-2 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
