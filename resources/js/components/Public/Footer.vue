446f77184621f00e8ad24607fe89023408f391f2
Xavier Au <xavier.au@gmail.com>
2025-10-19 11:31:34 +0800

    add contact us to footer

--- resources/js/components/Public/Footer.vue
+++ resources/js/components/Public/Footer.vue
@@ -4,6 +4,7 @@
             <ul class="flex justify-center space-x-4">
                 <li><a href="/pages/about-us" class="hover:underline">About Us</a></li>
                 <li><a href="/pages/privacy-policies" class="hover:underline">Privacy Policy</a></li>
+                <li><a href="/pages/contact-us" class="hover:underline">Contact Us</a></li>
                 <li><a href="/pages/terms-and-conditions" class="hover:underline">Terms and Conditions</a></li>
             </ul>
         </div>
